a49c9d48c829aac85b660fa31f12eee4a935db64
Dariusz Okonski <darek200180@gmail.com>
2021-05-14 11:46:19 +0100

    add test line

--- src/main/java/com/polishDraughts/App/App.java
+++ src/main/java/com/polishDraughts/App/App.java
@@ -3,5 +3,6 @@ package com.polishDraughts.App;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Second line");
     }
 }
