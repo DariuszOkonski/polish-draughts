a618f54fe6b1ea534c395db05b82106890a8307b
Daniel Wątor [KRK NOV 2020] <dmwator11@gmail.com>
2021-05-15 15:08:30 +0200

    delete comment

--- src/main/java/com/polishDraughts/App/App.java
+++ src/main/java/com/polishDraughts/App/App.java
@@ -2,15 +2,9 @@ package com.polishDraughts.App;
 
 public class App {
     public static void main(String[] args) {
-
-//        Board board = new Board(12);
-
-//        String[] data = InputGetter.getInputData();
-//        System.out.println(InputGetter.isInputValid(data));
         System.out.println(Board.INSTANCE);
         InputGetter.processMove();
 
-//        System.out.println(Arrays.deepToString(board.getFields()));
         Board.INSTANCE.print();
     }
 }
